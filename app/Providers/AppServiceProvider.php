c9ad4e5bacc3dc7e261700b138298e5ae6d0b48e
Mona Matar <mona@example.com>
2025-10-07 13:39:02 +0200

    Refactor AppServiceProvider to remove caching for global custom pages, directly querying the database instead.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,6 @@ use App\Models\Setting;
 use App\Models\CustomPage;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
-use Illuminate\Support\Facades\Cache;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,12 +25,10 @@ class AppServiceProvider extends ServiceProvider
         // Share setting globally with all views
         View::composer('*', function ($view) {
             $setting = Setting::firstOrCreate([]);
-            $customPage = Cache::remember('global_custom_pages', 3600, fn () =>
-            CustomPage::query()
-                    ->select('id','slug','page_name_en','page_name_ar')
-                    ->orderBy('page_name_en')
-                    ->get()
-            );
+            $customPage = CustomPage::query()
+                ->select('id','slug','page_name_en','page_name_ar')
+                ->orderBy('page_name_en')
+                ->get();
             View::share(compact('setting', 'customPage'));
         });
     }
